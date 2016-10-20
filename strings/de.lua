5e3e0fb13cb66f4c679155064195bfb128640928
Randactyl <rafowler@randactyl.com>
2016-10-20 12:44:37 -0700

    update german strings

--- strings/de.lua
+++ strings/de.lua
@@ -1,5 +1,5 @@
-SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_LABEL, "Item Tooltips versetzen", 1)
-SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_TOOLTIP, "Setzt die Item Tooltips links von der Scroll-Liste, so dass die das Gitter nicht verdecken.", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_LABEL, "Gegenstand-Tooltips versetzen", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_TOOLTIP, "Setzt die Gegenstand-Tooltips links von der Scroll-Liste, so dass die das Gitter nicht verdecken.", 1)
 
 SafeAddString(SI_INVENTORYGRIDVIEW_SKIN_DROPDOWN_LABEL, GetString(SI_COLLECTIBLECATEGORYTYPE11), 1)
 SafeAddString(SI_INVENTORYGRIDVIEW_SKIN_DROPDOWN_TOOLTIP, "Die für die Gitteransicht verwendeten Texturen.", 1)
@@ -8,7 +8,7 @@ SafeAddString(SI_INVENTORYGRIDVIEW_QUALITYOUTLINES_CHECKBOX_LABEL, "Qualität al
 SafeAddString(SI_INVENTORYGRIDVIEW_QUALITYOUTLINES_CHECKBOX_TOOLTIP, "Die Textur für die typische Umrahmung der Symbole im Gitter.", 1)
 
 SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_LABEL, "Minimale Qualität der Umrahmung", 1)
-SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_TOOLTIP, "Die Umrahmung wird nur für Items dieser oder höherer Qualität angezeigt.", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_TOOLTIP, "Die Umrahmung wird nur für Gegenstände dieser oder höherer Qualität angezeigt.", 1)
 
 SafeAddString(SI_INVENTORYGRIDVIEW_GRIDICONSIZE_SLIDER_LABEL, "Symbolgröße", 1)
 SafeAddString(SI_INVENTORYGRIDVIEW_GRIDICONSIZE_SLIDER_TOOLTIP, "Legt fest wie groß oder klein die Symbole sind. Die Symbolfläche ist das Quadrat von dieser Nummer.", 1)
