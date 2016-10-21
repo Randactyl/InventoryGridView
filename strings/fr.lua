33abea04b7c40f6713c11dbb7e30a073721840b7
Randactyl <rafowler@randactyl.com>
2016-10-20 17:50:12 -0700

    add french strings

---
+++ strings/fr.lua
@@ -0,0 +1,19 @@
+SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_LABEL, "Offset des tooltips", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_TOOLTIP, "Décaler les tooltip des objets vers la gauche de la fenêtre pour qu'ils n'empiètent pas sur la grille.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_SKIN_DROPDOWN_LABEL, GetString(SI_COLLECTIBLECATEGORYTYPE11), 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_SKIN_DROPDOWN_TOOLTIP, "Ensemble de textures à utiliser pour la grille.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_QUALITYOUTLINES_CHECKBOX_LABEL, "Bordures de qualité", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_QUALITYOUTLINES_CHECKBOX_TOOLTIP, "Une bordure plus marquée reflétant la qualité de l'objet sur les icônes de la grille.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_LABEL, "Qualité minimale pour les bordures", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_TOOLTIP, "Les bordures ne seront affichées que pour les objets possédant une qualité supérieure ou égale à cette valeur.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_GRIDICONSIZE_SLIDER_LABEL, "Taille des icônes", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_GRIDICONSIZE_SLIDER_TOOLTIP, "Définissez la taille des icônes sur la grille.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_ICONZOOMLEVEL_SLIDER_LABEL, "Niveau de zoom sur les icônes", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_ICONZOOMLEVEL_SLIDER_TOOLTIP, "Définissez le niveau de zoom appliqué lorsque la souris passe au-dessus de l'icône.", 1)
+
+SafeAddString(SI_BINDING_NAME_INVENTORYGRIDVIEW_TOGGLE, "Alterner entre la vue grille et la vue liste", 1)
