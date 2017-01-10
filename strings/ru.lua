7f3af4f52cc5e4be007f4fa5b210d249a43702f5
Randactyl <rafowler@randactyl.com>
2017-01-10 14:11:23 -0800

    add ru strings
    thanks Scope

---
+++ strings/ru.lua
@@ -0,0 +1,19 @@
+SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_LABEL, "Переместить подсказки для предметов", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_OFFSETITEMTOOLTIPS_CHECKBOX_TOOLTIP, "Расположить подсказки слева от списка предметов, так чтобы они не перекрывали их при переключении на вид сеткой.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_SKIN_DROPDOWN_LABEL, GetString(SI_COLLECTIBLECATEGORYTYPE11), 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_SKIN_DROPDOWN_TOOLTIP, "Набор текстур используемых для вида сеткой.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_QUALITYOUTLINES_CHECKBOX_LABEL, "Контуры качества", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_QUALITYOUTLINES_CHECKBOX_TOOLTIP, "Контуры с качеством будут отображаться на значках предметов.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_LABEL, "Минимальное качество для контура", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_MINOUTLINEQUALITY_DROPDOWN_TOOLTIP, "Отображать контуры только для указанного качества предметов или выше.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_GRIDICONSIZE_SLIDER_LABEL, "Размер значка", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_GRIDICONSIZE_SLIDER_TOOLTIP, "Установить размер значков. Площадь значка представляет собой квадрат этого числа.", 1)
+
+SafeAddString(SI_INVENTORYGRIDVIEW_ICONZOOMLEVEL_SLIDER_LABEL, "Уровень увеличения значка", 1)
+SafeAddString(SI_INVENTORYGRIDVIEW_ICONZOOMLEVEL_SLIDER_TOOLTIP, "Установить уровень увеличение значка (при наведении мышью) от отсутствующего до стандартного.", 1)
+
+SafeAddString(SI_BINDING_NAME_INVENTORYGRIDVIEW_TOGGLE, "Переключение вида сетка/список", 1)
